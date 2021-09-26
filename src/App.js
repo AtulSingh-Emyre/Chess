52ba0c3bbdcc64f15830878019889aaec2e85ade
Ati <190020009@iitdh.ac.in>
2021-09-26 15:01:53 +0530

    chess text to move UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import { Chessboard } from 'kokopu-react';
-import { useState } from 'react';
+import {Form} from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import Chess from 'chess.js';
+ var chess;
+
 function App() {
-  const [move, setmove] = useState("e4");
+  const [position, setPosition] = useState('start');
+  const [move, setmove] = useState('');
+  useEffect(() => {
+    chess = new Chess();
+    return () => {
+    };
+  }, []);
+  const HandleMove = (event) => {
+    setmove(event.target.value);
+    // console.log(event.target.value);
+  }
   const onClick = () => {
-    setmove("d4");
+    chess.move(move);
+    // chess.move('e5');
+    console.log(chess.fen());
+    setPosition(chess.fen());
+    setmove('');
   }
   return (
     <div className="App">
-      <Chessboard move ={move} />
+      <Chessboard position ={position} />
 
       <div onClick={() => onClick()} style={{height:200, width:100, backgroundColor:'black'}}>
+      <Form.Control style={{width:200, height: 80}} placeholder="Enter move to be played" onChange={HandleMove} value={move} />
 
       </div>
     </div>
