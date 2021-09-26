d118889050a44302104897d0f7870a45b7713a84
Ashish Akash <ashishakash333@gmail.com>
2021-09-26 18:21:51 +0530

    designed ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Chessboard } from 'kokopu-react';
-import {Form} from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import Chess from 'chess.js';
- var chess;
+var chess;
 
 function App() {
   const [position, setPosition] = useState('start');
@@ -26,11 +26,18 @@ function App() {
   }
   return (
     <div className="App">
-      <Chessboard position ={position} />
+      <div>
+        <h1>Speech Processing Course Project</h1>
+        <h4>Recognition of chess movements</h4>
+      </div>
+      <div style={{ margin: '25px' }}>
+        <Chessboard position={position} />
+      </div>
 
-      <div onClick={() => onClick()} style={{height:200, width:100, backgroundColor:'black'}}>
-      <Form.Control style={{width:200, height: 80}} placeholder="Enter the move to be played" onChange={HandleMove} value={move} />
-
+      <div >
+        <Form.Control type="text" style={{ height: '75px', width: '250px', textAlign: 'center' }} placeholder="Enter the move to be played" onChange={HandleMove} value={move} />
+        <br />
+        <Button style={{ height: '30px', width: '100px', backgroundColor: 'yellow', margin: '10px' }} onClick={() => onClick()}>Submit</Button>{' '}
       </div>
     </div>
   );
