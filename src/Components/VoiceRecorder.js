5f39095bd21a4e84e936a9f73150fdb571a175e9
Ati <190020009@iitdh.ac.in>
2021-10-02 19:29:16 +0530

    mfcc coefficients trial 1

--- src/Components/VoiceRecorder.js
+++ src/Components/VoiceRecorder.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 // Import module.
 
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+import Meyda from "meyda";
+const audioContext = new AudioContext();
 
 
 const Voicerecorder = () => {
     const [recordState, setrecordState] = useState(null);
-
+    const audioRef = useRef();
     const onStart = () => {
         setrecordState(RecordState.START);
     }
@@ -16,7 +18,7 @@ const Voicerecorder = () => {
 
     //audioData contains blob and blobUrl
     const onStop = (audioData) => {
-        console.log('audioData', audioData)
+        console.log('audioData >>>', audioData);
     }
 
     
@@ -24,7 +26,16 @@ const Voicerecorder = () => {
         <div>
             <div style={{backgroundColor:'white',width:300,height:300}}>
             <AudioReactRecorder state={recordState} onStop={onStop} />
-            
+            <audio controls ref={audioRef}>
+                <source src={recordState?recordState.url:null} type='audio/mpeg' />
+            </audio>
+            {/* <audio
+                controls
+                loop
+                crossorigin="anonymous"
+                id="audio"
+                src={recordState?recordState.url:null}
+            ></audio> */}
             </div>
             <div style={{backgroundColor:'black',width:300,height:300}} onClick={onStart}></div>
             <div style={{backgroundColor:'yellow',width:300,height:300}} onClick={onEnd}></div>
